811065257c64ca379bda7a34b955d968629e65be
JoanRr <joarru12@htl-shkoder.com>
2017-06-02 23:47:53 +0200

    Update dbconnect.php
    Wir haben den Datenbank geaendert. mydb-->webshop

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $hostname='localhost';
 $user = 'root';
 $password = '';
-$mysql_database = 'mydb';
+$mysql_database = 'webshop';
 $db = mysql_connect($hostname, $user, $password,$mysql_database);
-mysql_select_db("mydb", $db);
+mysql_select_db("webshop", $db);
 ?>
