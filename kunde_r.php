43b2703c2dbbe3d310808218b2a7c48653354ddc
JoanRr <joarru12@htl-shkoder.com>
2017-06-04 01:11:00 +0200

    +Komentieren

--- kunde_r.php
+++ kunde_r.php
@@ -15,7 +15,7 @@
 				<!-- Header -->
 					<header id="header">
 						<h1>Kunde Registrieren</h1>
-						<p>Registrieren Sie bitte...</p>
+						<p>Registrieren Sie sich bitte...</p>
 					</header>
 					<nav id="nav">
 						<ul>
@@ -31,7 +31,8 @@
 							<section id="content" class="main">
 								<span class="image main"><img src="images/webshop.jpg" alt="" /></span>
 								<h2>Kunde Registrieren</h2>
-								<form action="registerK.php" method="post">
+								<!--Erstellung eines Formulars, mit Feld-Namen, wie die Attributten in DB -->
+								<form action="kunde_r.php" method="post">
 					Vorname: <br> <input type="text" size="40" maxlength="250" name="vorname"><br><br>
 					Nachname: <br> <input type="text" size="40" maxlength="250" name="nachname"><br><br>
 					Email: <br> <input type="text" size="40" maxlength="250" name="email"><br><br>
@@ -43,11 +44,18 @@
 					Telefonnummer: <br> <input type="text" size="40" maxlength="250" name="telefonnummer"><br><br>
 					Benutzername: <br> <input type="text" size="40" maxlength="250" name="benutzername"><br><br>
 					Passwort:<br> <input type="password" size="40"  maxlength="250" name="passwort"><br>
-					<button name="Send" type="submit">Submit</button>
+
+					<!-- Erstellung von Send-Button -->
+					<button name="Send" type="submit">Abschicken</button>
+					<!-- Erstellung von Reset-Button -->
+					<button name="reset" type="reset">Reset</button>
 				</form>
 			<?php 
+			/*Funktionalitaet des Send-Buttons*/
 			if(isset($_POST["Send"])){
-				$pdo = new PDO('mysql:host=localhost;port=3306;dbname=mydb', 'root', '');
+				/*Verbindung mit der DB*/
+				$pdo = new PDO('mysql:host=localhost;port=3306;dbname=webshop', 'root', '');
+				/*Werte in die Variable speichern*/
 				$vorname = $_POST["vorname"];
 				$nachname = $_POST["nachname"];
 				$email = $_POST["email"];
@@ -59,11 +67,13 @@
 				$telefonnummer = $_POST["telefonnummer"];
 				$benutzername = $_POST["benutzername"];
 				$passwort = $_POST["passwort"];
-			
+				
+				/*Insert Statement*/
 				$insert = "INSERT INTO kunde (Vorname, Nachname, Email, Geburtstag, HausNr, Land, Stadt, Strasse, Telefonnummer, benutzername, passwort)
 							VALUES('".$vorname."','".$nachname."','".$email."','".$geburtstag."','".$hausnr."','".$land."','".$stadt."','".$strasse."','".$telefonnummer."','".$benutzername."','".$passwort."')";
+				/*Durchfuehrung von Insert-Statement*/
 				$result = $pdo->query($insert);
-			
+				/*Pruefen, ob die Datensaetze angelegt sind*/
 				if($result)
 					echo "<br />Der User wurde erfolgreich angelegt!";
 			
@@ -72,10 +82,6 @@
 										}
 			?>
 							</section>
-
-					
-
-				
 
 			</div>
 
